7968fb32d0bea3736b72e044cce4ef1579aa3f2d
escg91 <elianacg17@gmail.com>
2023-01-04 18:19:32 +0000

    Use JS to validate an HTML Form

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    //write your code here
+    let form = document.querySelector("form");
+    form.addEventListener("submit", enviarDatos);
 };
+
+function enviarDatos(e) {
+    e.preventDefault(); // detiene tu comportamiento predeterminado del formulario e interpretado mi codigo
+    let card = document.getElementById("card");
+    let cvc = document.getElementById("cvc");
+    let amount = document.getElementById("amount");
+    let FirstName = document.getElementById("FirstName");
+    let LastName = document.getElementById("LastName");
+    let city = document.getElementById("city");
+    let state = document.getElementById("state");
+    let PostalCode = document.getElementById("PostalCode");
+    let message = document.getElementById("message");
+    let alert = document.getElementById("aler alert-danger");
+
+    console.log(card.value);
+    console.log(cvc.value);
+    console.log(amount.value);
+    console.log(FirstName.value);
+    console.log(LastName.value);
+    console.log(city.value);
+    console.log(state.value);
+    console.log(PostalCode.value);
+    console.log(message.value);
+
+    if (card === "") {
+        //console.log("El campo email está vacio");
+        document.getElementById("card").style.backgroud = "red";
+        document.getElementById("alert");
+    } else {
+        document.getElementById("alert") = hidden
+    }
