4618ff0b98e102ab54ed79d8c13986b5b39f9b77
escg91 <elianacg17@gmail.com>
2023-01-10 12:18:05 +0000

    corregido

--- src/app.js
+++ src/app.js
@@ -6,38 +6,110 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-    //write your code here
-    let form = document.querySelector("form");
-    form.addEventListener("submit", enviarDatos);
-};
+  //write your code here
+  let form = document.getElementById("form");
+  form.addEventListener("submit", enviarDatos);
 
-function enviarDatos(e) {
-    e.preventDefault(); // detiene tu comportamiento predeterminado del formulario e interpretado mi codigo
+  function enviarDatos(e) {
+    e.preventDefault();
     let card = document.getElementById("card");
     let cvc = document.getElementById("cvc");
     let amount = document.getElementById("amount");
-    let FirstName = document.getElementById("FirstName");
-    let LastName = document.getElementById("LastName");
+    let firstName = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
     let city = document.getElementById("city");
     let state = document.getElementById("state");
-    let PostalCode = document.getElementById("PostalCode");
+    let postalCode = document.getElementById("postalCode");
     let message = document.getElementById("message");
-    let alert = document.getElementById("aler alert-danger");
+    let alert = document.getElementById("alert");
 
-    console.log(card.value);
-    console.log(cvc.value);
-    console.log(amount.value);
-    console.log(FirstName.value);
-    console.log(LastName.value);
-    console.log(city.value);
-    console.log(state.value);
-    console.log(PostalCode.value);
-    console.log(message.value);
-
-    if (card === "") {
-        //console.log("El campo email está vacio");
-        document.getElementById("card").style.backgroud = "red";
-        document.getElementById("alert");
-    } else {
-        document.getElementById("alert") = hidden
+    //cambiando color de fondo a Card
+    if (card.value === "") {
+      card.style.background = "salmon";
+      //alert
+      alert.style.display = "block";
     }
+
+    //cambiando color de fondo a CVC
+    if (cvc.value === "") {
+      cvc.style.background = "salmon";
+      //alert
+      alert.style.display = "block";
+    }
+
+    //cambiando color de fondo a amount
+    if (amount.value === "") {
+      amount.style.background = "salmon";
+      //alert
+      alert.style.display = "block";
+    }
+
+    //cambiando color de fondo a firstName
+    if (firstName.value === "") {
+      firstName.style.background = "salmon";
+      //alert
+      alert.style.display = "block";
+    }
+
+    //cambiando color de fondo a lastName
+    if (lastName.value === "") {
+      lastName.style.background = "salmon";
+      //alert
+      alert.style.display = "block";
+    }
+
+    //cambiando color de fondo a City
+    if (city.value === "") {
+      city.style.background = "salmon";
+      //alert
+      alert.style.display = "block";
+    }
+
+    //cambiando color de fondo a State
+    if (state.value === "Pick a State") {
+      state.style.background = "salmon";
+      //alert
+      alert.style.display = "block";
+    }
+
+    //cambiando color de fondo al Codigo postal
+    if (postalCode.value === "") {
+      postalCode.style.background = "salmon";
+      //alert
+      alert.style.display = "block";
+    }
+
+    //cambiando color de fondo al Mensaje
+    if (message.value === "") {
+      message.style.background = "salmon";
+      //alert
+      alert.style.display = "block";
+    }
+
+    //funcion para validar que los campos no esten vacios
+
+    /*const expresiones = {
+                                              texto: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+                                              numeros: /^\d{4,14}$/ // 7 a 14 numeros.
+                                            };*/
+
+    /*function validateFields(e) {
+                                      let field = e.target;
+                                      let fieldValue = e.target.value;
+                                      if (fieldValue.trim().length === 0 && stateValue === "Pick a State") {
+                                        field.style.background = "salmon";
+                                        alert.style.display = "block";
+                                      }
+                                    }
+                                    card.addEventListener("blur", validateFields);
+                                    cvc.addEventListener("blur", validateFields);
+                                    amount.addEventListener("blur", validateFields);
+                                    firstName.addEventListener("blur", validateFields);
+                                    lastName.addEventListener("blur", validateFields);
+                                    city.addEventListener("blur", validateFields);
+                                    state.addEventListener("blur", validateFields);
+                                    postalCode.addEventListener("blur", validateFields);
+                                    message.addEventListener("blur", validateFields);
+                                    alert.addEventListener("blur", validateFields);*/
+  }
+};
